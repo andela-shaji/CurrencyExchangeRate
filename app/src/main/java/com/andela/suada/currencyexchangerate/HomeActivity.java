e8a7ec4a65ac97c76bb46707e30acb832159a1bb
Suada Haji <suada.haji@andela.com>
2016-07-04 17:45:00 +0300

    Create the two activities required for selecting, calcuting and displaying the currency

---
+++ app/src/main/java/com/andela/suada/currencyexchangerate/HomeActivity.java
@@ -0,0 +1,43 @@
+package com.andela.suada.currencyexchangerate;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+
+public class HomeActivity extends AppCompatActivity {
+    private EditText edExchangeValue;
+    private Spinner spinnerCurrency;
+    private Button searchButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        edExchangeValue = (EditText) findViewById(R.id.etexchangeValue);
+        spinnerCurrency = (Spinner) findViewById(R.id.currencySpinner);
+        searchButton = (Button) findViewById(R.id.currencySearch);
+
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String valueCurrency = edExchangeValue.getText().toString();
+                if (!valueCurrency.isEmpty()) {
+                    String sendRate = spinnerCurrency.getSelectedItem().toString() + ":" + valueCurrency;
+                    Intent sendIntent = new Intent(getBaseContext(), ResultActivity.class);
+                    sendIntent.putExtra("RATES", sendRate);
+                    startActivity(sendIntent);
+
+                }
+            }
+        });
+    }
+
+}
+
+
+
